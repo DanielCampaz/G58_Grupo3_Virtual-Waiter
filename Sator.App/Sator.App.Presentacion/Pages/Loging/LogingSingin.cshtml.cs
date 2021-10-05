e1b5bb663635d3a132a40a1ba57df0fd8200a8c9
JhonnyValdivieso <jhonnyrevall@gmail.com>
2021-10-04 22:21:06 -0500

    Updated

--- Sator.App/Sator.App.Presentacion/Pages/Loging/LogingSingin.cshtml.cs
+++ Sator.App/Sator.App.Presentacion/Pages/Loging/LogingSingin.cshtml.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace Sator.App.Presentacion.Pages
 {
     public class LogingSinginModel : PageModel
     {
+        private readonly ILogger<LogingSinginModel> _logger;
+
+        public LogingSinginModel(ILogger<LogingSinginModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
         }
