619f2f71b9da3b357791f198e70bce3b477d5ea0
Daniela Robles Urbina <danisru02@hotmail.com>
2021-10-10 20:21:16 -0500

    "Creación de capa de la consola"

---
+++ Sator.App/Sator.App.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Sator.App.Dominio;
+
+namespace Sator.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
